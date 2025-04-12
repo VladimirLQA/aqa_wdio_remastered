f4f5c7521db3aaf0fb5cbef6de676d76cbe3dd97
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2025-04-12 21:13:06 +0300

    organizing imports

---
+++ src/ui/services/index.services.ts
@@ -0,0 +1,7 @@
+import HomePageService from './homePage.service';
+import ProductsPageService from './products/productsPage.service';
+import AddNewProductService from './products/addNewProductPage.service';
+import EditProductService from './products/editProductPage.service';
+import SignInPageService from './signInPage.service';
+
+export { SignInPageService, ProductsPageService, HomePageService, AddNewProductService, EditProductService };
